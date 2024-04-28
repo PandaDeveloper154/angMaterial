d8ac3d57f4e2c3b5b2ab5dba38075aa1045b9add
PandaDeveloper154 <168059413+PandaDeveloper154@users.noreply.github.com>
2024-04-28 13:48:45 +0700

    EcomAngular
    have season 1-20

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngMaterial
+# EcommerceProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.11.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ang-material'),
+      dir: require('path').join(__dirname, './coverage/ecommerce-project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
